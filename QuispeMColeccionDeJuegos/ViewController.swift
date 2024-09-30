7ddf7355243c4a76c7000167f9a3c8f2122a9048
dquispe <david.quispe.m@tecsup.edu.pe>
2024-09-30 17:11:53 -0500

    Initial Commit

---
+++ QuispeMColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuispeMColeccionDeJuegos
+//
+//  Created by David Quispe Maqque on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
